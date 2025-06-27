54eb534055e5df2475880f1ac4d23af1057d7579
tipa.git <tipa.git@gmail.com>
2025-06-27 11:06:20 +0700

    new layout

--- app/(tipa)/dashboard/updashboard/page.tsx
+++ app/(tipa)/dashboard/updashboard/page.tsx
@@ -8,7 +8,7 @@ const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className="w-full lg:w-3/4 flex flex-col gap-8">
+      <div className="w-full lg:w-3/4 md:w-3/4 flex flex-col gap-8">
         <div className="flex gap-2 flex-col lg:flex-row">
           <div className="w-full min-w-[300px] lg:w-1/3 max-h-[500px]">
             <CountChart />
@@ -18,16 +18,16 @@ const AdminPage = () => {
           </div>
         </div>
         <div className="flex gap-2 flex-col lg:flex-row">
-          <div className="w-full min-w-[350px] lg:w-1/2 max-h-[500px]">
+          <div className="w-full min-w-[450px] md:w-2/5 lg:w-2/5 max-h-[500px]">
             <DayFrameGroupChart />
           </div>
-          <div className="w-full min-w-[450px] lg:w-1/2 max-h-[500px]">
+          <div className="w-full min-w-[450px] md:w-3/5 lg:w-3/5 max-h-[500px]">
             <FrameGroupChart />
           </div>
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/4 flex flex-col gap-8">
+      <div className="w-full lg:w-1/4 md:w-3/4 flex flex-col gap-8">
         <CustomerCards />
         <Announcements />
       </div>
